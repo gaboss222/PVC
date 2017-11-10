d04f74aa389ec8078d3fe35df995901132fe936b
Guillaume Noguera <guillaume.noguera@gmail.com>
2017-11-10 16:05:09 +0100

    added naive search

--- main.py
+++ main.py
@@ -13,20 +13,38 @@ class City:
     def __str__(self):
         return "name : {0}\n coords : ({1},{2})".format(self.n, self.x, self.y)
     '''static func to return dist between two cities '''
-    def dist(v1, v2):
+    def score(v1, v2):
         return sqrt(pow(v2.x - v1.x, 2)+pow(v2.y - v1.y, 2))
 
 class Solution:
-    def __init__(self, problem):
-        self.sol = random.sample(problem, len(problem))
-        for s in self.sol:
-            print(str(s))
+    def __init__(self, problem, init = True):
+        if (init):
+            self.sol = random.sample(problem, len(problem))
+        else:
+            self.sol = list()
 
-    def getTotalDistance(self):
+    def score(self):
         tot = 0
         for i in range(len(self.sol) - 1):
-            tot+= City.dist(self.sol[i], self.sol[i+1])
+            tot+= City.score(self.sol[i], self.sol[i+1])
         return tot
+
+    def legal(self):
+        return len(self.sol) is len(set(self.sol))   #https://stackoverflow.com/questions/5278122/checking-if-all-elements-in-a-list-are-unique
+
+    def crossover(self, s1, s2):
+        child = None
+        #todo
+
+    def swap(sol1, sol2, index):
+        temp = Solution(problem, False)
+        for i in range(len(sol1.sol)):
+            if i < index:
+                temp.sol.append(sol1.sol[i])
+            else:
+                temp.sol.append(sol2.sol[i])
+        return temp
+
 
 
 #populating our city list
@@ -37,5 +55,12 @@ with open('data/pb005.txt') as f:
         words = line.split()
         problem.append(City(words[0], int(words[1]), int(words[2])))
 
-s = Solution(problem)
-print("Distance totale : {:f}".format(s.getTotalDistance()))
+sols = list()
+for i in range(0, 1000):
+    sols.append(Solution(problem))
+
+sols.sort(key=lambda sol : sol.score())
+
+print(sols[0].score())
+for e in sols[0].sol:
+    print(e.n)
