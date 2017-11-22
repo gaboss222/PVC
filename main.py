1c2e09ab30901553d46e1fa2036722cf2601df4f
Guillaume Noguera <guillaume.noguera@gmail.com>
2017-11-22 14:09:31 +0100

    line display

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from math import sqrt, pow
 import random
 import pygame
 
+white = (255,255,255)
+
+
 class City:
     def __init__(self, n, x, y):
         self.n = n
@@ -12,20 +15,20 @@ class City:
     '''static func to return dist between two cities '''
     def score(v1, v2):
         return sqrt(pow(v2.x - v1.x, 2)+pow(v2.y - v1.y, 2))
-        
+
     def tailleProblem(self):
         return len(self.problem)
 
-    
+
 def mutation(cities):
     for c in cities:
         #print(c)
         if random.random() < 0.015:
             c2 = int(len(cities) * random.random())
-            
-            
-        
-        
+
+
+
+
 class Solution:
     def __init__(self, problem, init = True):
         if (init):
@@ -46,6 +49,11 @@ class Solution:
     def legal(self):
         return len(self.sol) is len(set(self.sol))   #source : https://stackoverflow.com/questions/5278122/checking-if-all-elements-in-a-list-are-unique
 
+    def screenPrint(self, screen):
+        for i in range(len(self.sol) -1):
+            pygame.draw.line(screen, white, (self.sol[i].x, self.sol[i].y), (self.sol[i+1].x, self.sol[i+1].y), 2)
+
+
 if __name__ == "__main__":
     #usage : python main.py [time limit] [maximum gen] [path]
     import sys
@@ -56,8 +64,8 @@ if __name__ == "__main__":
 
     timelimit = float(sys.argv[1])
     print("maxGen : {:d}".format(maxGen))
-    
-    white = (255,255,255)
+
+
     circle_color = (100,200,200)
     (width, height) = (500, 500)
     screen = pygame.display.set_mode((width, height))
@@ -73,7 +81,7 @@ if __name__ == "__main__":
                 #print city on screen
                 pygame.draw.circle(screen, circle_color, (e.x, e.y), 30, 3)
                 pygame.display.flip()
-                    
+
 
     else:
         running = True
@@ -83,24 +91,40 @@ if __name__ == "__main__":
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x,y = pygame.mouse.get_pos()
                     problem.append(City("v{:d}".format(len(problem)), x, y))
-                    
+
                 #if event.type is pygame.KEYDOWN and event.key is pygame.K_ESCAPE:
-                    #Run gen. Algo  
+                    #Run gen. Algo
+
                 if event.type == pygame.QUIT:
                     running = False
-                    
+
             for c in problem:
                 pygame.draw.circle(screen, circle_color, (c.x, c.y), 10, 3)
             pygame.display.flip()
 
-    mutation(problem)
-    
+    #start searching
+    solutions = list()
     time.clock()
+    for i in range(1000):
+        solutions.append(Solution(problem))
+
+    solutions_sorted = sorted(solutions, key=lambda x : x.score())
+
+    for i in range(10):
+        print(solutions_sorted[i].score())
+    pygame.display.flip()
+
+    solutions_sorted[0].screenPrint(screen)
+
+    pygame.display.flip()
+    mutation(problem)
+
+
     nGen = 0
     while True:
         #Search happens there
         if time.clock() >= timelimit or nGen >= maxGen:
             break
         nGen+=1
-        
+
     print("Search ended after {:f} seconds and {:d} generations".format(time.clock(), nGen))
